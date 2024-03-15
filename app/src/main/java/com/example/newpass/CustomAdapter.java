c70476929858cfbf52d642057847b843ee34fffb
gero <gerotto.alessandro08@gmail.com>
2024-03-15 22:24:16 +0100

    Graphic changes

--- app/src/main/java/com/example/newpass/CustomAdapter.java
+++ app/src/main/java/com/example/newpass/CustomAdapter.java
@@ -3,6 +3,7 @@ package com.example.newpass;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     private Context context;
     private ArrayList row_id, row_name, row_email, row_password;
     private Activity activity;
+    private String tw, name, email;
 
     CustomAdapter(Activity activity, Context context, ArrayList row_id, ArrayList row_name, ArrayList row_email, ArrayList row_password) {
         this.activity = activity;
@@ -37,12 +39,27 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
         return new MyViewHolder(view);
     }
 
+    /**
+     * Populate every row of the recyclerView in the main activity
+     * @param holder The ViewHolder which should be updated to represent the contents of the
+     *        item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, final int position) {
-        holder.row_id_txt.setText(String.valueOf(row_id.get(position)));
-        holder.row_name_txt.setText(String.valueOf(row_name.get(position)));
-        holder.row_email_txt.setText(String.valueOf(row_email.get(position)));
-        //holder.row_password_txt.setText(String.valueOf(row_password.get(position)));
+
+        name = String.valueOf(row_name.get(holder.getAdapterPosition()));
+        email = String.valueOf(row_email.get(holder.getAdapterPosition()));
+
+        if (name.length() > 2) {
+            tw = name.substring(0, 2);
+        } else {
+            tw = name;
+        }
+
+        holder.row_tw_txt.setText(tw);
+        holder.row_name_txt.setText(name);
+        holder.row_email_txt.setText(email);
 
         holder.mainLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,6 +74,10 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
         });
     }
 
+    /**
+     * Get the number of password saved in the database
+     * @return the number of password saved in the database
+     */
     @Override
     public int getItemCount() {
         return row_id.size();
@@ -64,13 +85,14 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
-        TextView row_id_txt, row_name_txt, row_email_txt, row_password_txt;
+        TextView row_id_txt, row_name_txt, row_email_txt, row_password_txt, row_tw_txt;
         LinearLayout mainLayout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            row_id_txt = itemView.findViewById(R.id.row_id_txt);
+            //row_id_txt = itemView.findViewById(R.id.row_id_txt);
             row_name_txt = itemView.findViewById(R.id.row_name_txt);
+            row_tw_txt = itemView.findViewById(R.id.row_tw_txt);
             row_email_txt = itemView.findViewById(R.id.row_email_txt);
             //row_password_txt = itemView.findViewById(R.id.row_password_txt);
             mainLayout = itemView.findViewById(R.id.mainLayout);
