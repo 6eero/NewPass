d1daea48614564cf9663b5555b726b42b1acb0a5
gero <gerotto.alessandro08@gmail.com>
2024-03-16 15:56:33 +0100

    added EncryptionHelper class

---
+++ app/src/main/java/com/example/newpass/EncryptionHelper.java
@@ -0,0 +1,37 @@
+package com.example.newpass;
+
+import android.util.Base64;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class EncryptionHelper {
+    private static final String ALGORITHM = "Blowfish";
+    private static final String MODE = "Blowfish/CBC/PKCS5Padding";
+    private static final String IV = "abcdefgh";
+    private static final String Key = "negro"; //Replace this with your custom key
+
+    public static String encrypt(String value) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        SecretKeySpec secretKeySpec = new SecretKeySpec(Key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(MODE);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, new IvParameterSpec(IV.getBytes()));
+        byte[] values = cipher.doFinal(value.getBytes());
+        return Base64.encodeToString(values, Base64.DEFAULT);
+    }
+
+    public static String decrypt(String value) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] values = Base64.decode(value, Base64.DEFAULT);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(Key.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(MODE);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new IvParameterSpec(IV.getBytes()));
+        return new String(cipher.doFinal(values));
+    }
+}
