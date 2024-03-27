1c4239c8e90504c14ab4a72d42ecb94695ef011e
gero <gerotto.alessandro08@gmail.com>
2024-03-27 21:58:40 +0100

    Fixed the DPI settings issue that made the app unusable on devices with small screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.gero.newpass"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
